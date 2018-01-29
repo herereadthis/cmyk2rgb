10cac62e8d29b8a9f79584a67db92f140f9e1585
herereadthis <herereadthis@gmail.com>
2018-01-28 23:02:50 -0500

    figured out intents

--- myColorSkill/lambda_function.py
+++ myColorSkill/lambda_function.py
@@ -11,9 +11,11 @@ For additional samples, visit the Alexa Skills Kit Getting Started guide at
 http://amzn.to/1LGWsLG
 """
 
+APPLICATION_ID = 'amzn1.ask.skill.a93ba60c-e4f2-42a5-b08a-f5b8ddbf6f44'
+
 # --------------- Helpers that build all of the responses ---------------------
 
-
+ 
 def build_speechlet_response(title, output, reprompt_text, should_end_session):
     return {
         'outputSpeech': {
@@ -43,7 +45,7 @@ def build_response(session_attributes, speechlet_response):
     }
 
 
-# --------------- Functions that control the skill's behavior ------------------
+# --------------- Functions that control the skill's behavior -----------------
 
 
 def get_welcome_response():
@@ -53,11 +55,14 @@ def get_welcome_response():
 
     session_attributes = {}
     card_title = "Welcome"
-    speech_output = "Welcome to the Alexa Skills Kit sample. " \
-                    "Please tell me your favorite color by saying, " \
-                    "my favorite color is red"
-    # If the user either does not reply to the welcome message or says something
-    # that is not understood, they will be prompted again with this text.
+    speech_output = """
+    Welcome to the color fox 7. Tell me your favorite color.
+    Jimmy, you are prettier than your wife Uyen.
+    """
+
+    # If the user either does not reply to the welcome message or says
+    # something that is not understood, they will be prompted again with this
+    # text.
     reprompt_text = "Please tell me your favorite color by saying, " \
                     "my favorite color is red."
     should_end_session = False
@@ -67,7 +72,7 @@ def get_welcome_response():
 
 def handle_session_end_request():
     card_title = "Session Ended"
-    speech_output = "Thank you for trying the Alexa Skills Kit sample. " \
+    speech_output = "Thank you for playing the color fox. " \
                     "Have a nice day! "
     # Setting this to true ends the session and exits the skill.
     should_end_session = True
@@ -114,6 +119,7 @@ def get_color_from_session(intent, session):
     if session.get('attributes', {}) and "favoriteColor" in session.get('attributes', {}):
         favorite_color = session['attributes']['favoriteColor']
         speech_output = "Your favorite color is " + favorite_color + \
+                        "session" + str(session_attributes) + \
                         ". Goodbye."
         should_end_session = True
     else:
@@ -158,55 +164,60 @@ def on_intent(intent_request, session):
     intent_name = intent_request['intent']['name']
 
     # Dispatch to your skill's intent handlers
-    if intent_name == "MyColorIsIntent":
+    if intent_name == "AMAZON.CancelIntent":
+        return handle_session_end_request()
+    elif intent_name == "AMAZON.StopIntent":
+        return handle_session_end_request()
+    elif intent_name == "MyColorIsIntent":
         return set_color_in_session(intent, session)
     elif intent_name == "WhatsMyColorIntent":
         return get_color_from_session(intent, session)
     elif intent_name == "AMAZON.HelpIntent":
         return get_welcome_response()
-    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
-        return handle_session_end_request()
     else:
         raise ValueError("Invalid intent")
 
 
 def on_session_ended(session_ended_request, session):
-    """ Called when the user ends the session.
-
+    """Called when the user ends the session."""
+    """
     Is not called when the skill returns should_end_session=true
     """
     print("on_session_ended requestId=" + session_ended_request['requestId'] +
           ", sessionId=" + session['sessionId'])
     # add cleanup logic here
-
-
-# --------------- Main handler ------------------
+    return handle_session_end_request()
 
 
 def lambda_handler(event, context):
-    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
+    """Main Handler skill."""
+    """
+    Route the incoming request based on type (LaunchRequest, IntentRequest,
     etc.) The JSON body of the request is provided in the event parameter.
     """
-    print("event.session.application.applicationId=" +
-          event['session']['application']['applicationId'])
+    application_id = event['session']['application']['applicationId']
 
-    """
-    Uncomment this if statement and populate with your skill's application ID
-    to
-    prevent someone else from configuring a skill that sends requests to this
-    function.
-    """
-    # if (event['session']['application']['applicationId'] !=
-    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
-    #     raise ValueError("Invalid Application ID")
+    print('event.session.application.applicationId={}'.format(application_id))
+
+    if application_id != APPLICATION_ID:
+        """
+        Make sure the event's application ID is the same as the skill's
+        application ID, to prevent someone else from configuring a skill that
+        sends requests to this function.
+        """
+        raise ValueError("Invalid Application ID")
 
     if event['session']['new']:
         on_session_started({'requestId': event['request']['requestId']},
                            event['session'])
 
-    if event['request']['type'] == "LaunchRequest":
-        return on_launch(event['request'], event['session'])
-    elif event['request']['type'] == "IntentRequest":
-        return on_intent(event['request'], event['session'])
-    elif event['request']['type'] == "SessionEndedRequest":
-        return on_session_ended(event['request'], event['session'])
+    event_type = event['request']['type']
+
+    if event_type == "LaunchRequest":
+        handler = on_launch
+    elif event_type == "IntentRequest":
+        handler = on_intent
+    elif event_type == "SessionEndedRequest":
+        handler = on_session_ended
+
+    return handler(event['request'], event['session'])
