7776fe407af63c240e0e4fc93f8f33af76696660
herereadthis <herereadthis@gmail.com>
2018-02-12 23:42:37 -0500

    kwargs

--- myColorSkill/lambda_function.py
+++ myColorSkill/lambda_function.py
@@ -61,6 +61,14 @@ RESPONSES = {
         'reprompt_text': None,
         'should_end_session': True
     },
+    'continue_end_ambituity': {
+        'card_title': 'ContinueEndAmbiguity',
+        'speech_output': """
+            I\'m not sure what you want.
+            """,
+        'reprompt_text': None,
+        'should_end_session': True
+    },
     'help': {
         'card_title': 'Session Help',
         'speech_output': """
@@ -141,6 +149,29 @@ def build_unformatted_speechlet_response(response_type):
     return get_lambda_output(response)
 
 
+def get_response_alt(response_type, **kwargs):
+    """Create the response for Alexa to interpret."""
+    try:
+        responses = RESPONSES[response_type]
+        speech_output = responses['speech_output']
+        reprompt_text = responses['reprompt_text']
+        if 'speech_output' in kwargs:
+            speech_output = kwargs['speech_output']
+        if 'reprompt_text' in kwargs:
+            reprompt_text = kwargs['reprompt_text']
+    except:
+        responses = RESPONSES['help']
+        speech_output = responses['speech_output']
+        reprompt_text = responses['reprompt_text']
+
+    responses = RESPONSES[response_type]
+    card_title = responses['card_title']
+    should_end_session = responses['should_end_session']
+
+    return get_response(card_title, speech_output, reprompt_text,
+                        should_end_session)
+
+
 def get_response(title, output, reprompt_text, should_end_session):
     """Create the response for Alexa to interpret."""
     return {
@@ -197,23 +228,11 @@ def handle_session_end_request(intent, session):
     """Create the response when ending exiting the app."""
     try:
         favorite_color = session['attributes']['favoriteColor']
+        output = RESPONSES['end_session_known_color']['speech_output']
+        output = output.format(favorite_color, favorite_color)
 
-        # card_title = 'end session known color'
-        # speech_output = 'end known color {}'.format(favorite_color)
-        # reprompt_text = None
-        # should_end_session = True
-
-        responses = RESPONSES['end_session_known_color']
-
-        card_title = intent['name']
-        # speech_output = 'end known color {}'.format(favorite_color)
-        speech_output = responses['speech_output'].format(favorite_color,
-                                                          favorite_color)
-        reprompt_text = responses['reprompt_text']
-        should_end_session = responses['should_end_session']
-
-        response = get_response(card_title, speech_output,
-                                reprompt_text, should_end_session)
+        response = get_response_alt('end_session_known_color',
+                                    speech_output=output)
         return get_lambda_output(response)
     except KeyError:
         return build_unformatted_speechlet_response(
@@ -227,15 +246,8 @@ def handle_help_request():
 
 def handle_continue_end_ambiguity_request(intent, session):
     """Create response when user says yes or no at the wrong time."""
-    session_attributes = {}
-    card_title = 'ContinueEndAmbiguity'
-    speech_output = 'I\'m not sure what you want.'
-    reprompt_text = None
-    should_end_session = True
-
-    response = get_response(card_title, speech_output,
-                            reprompt_text, should_end_session)
-    return get_lambda_output(response, session_attributes)
+    # TODO: handle yes and no.
+    return build_unformatted_speechlet_response('continue_end_ambituity')
 
 
 def handle_yes_request(intent, session):
@@ -273,17 +285,19 @@ def handle_no_request(intent, session):
     try:
         favorite_color = session['attributes']['favoriteColor']
         continue_prompt_asked = session['attributes']['continuePromptAsked']
+        if strtobool(continue_prompt_asked) == 1:
+            result = handle_session_end_request(intent, session)
+        else:
+            session_attributes = {}
+            card_title = 'EndSessionIntent'
+            speech_output = 'no no no {}'.format(favorite_color,
+                                                 continue_prompt_asked)
+            reprompt_text = None
+            should_end_session = True
 
-        session_attributes = {}
-        card_title = 'EndSessionIntent'
-        speech_output = 'no no no {}'.format(favorite_color,
-                                             continue_prompt_asked)
-        reprompt_text = None
-        should_end_session = True
-
-        response = get_response(card_title, speech_output, reprompt_text,
-                                should_end_session)
-        result = get_lambda_output(response, session_attributes)
+            response = get_response(card_title, speech_output, reprompt_text,
+                                    should_end_session)
+            result = get_lambda_output(response, session_attributes)
     except KeyError:
         result = handle_continue_end_ambiguity_request(intent, session)
 
@@ -292,8 +306,6 @@ def handle_no_request(intent, session):
 
 def set_color_in_session(intent, session):
     """Set the color in the session and prepare the reply speech to user."""
-    card_title = intent['name']
-
     if 'Color' in intent['slots']:
         color = intent['slots']['Color']
         favorite_color = color['value']
@@ -303,14 +315,11 @@ def set_color_in_session(intent, session):
 
     if code == CODES['match']:
         session_attributes = create_favorite_color_attributes(favorite_color)
-        responses = RESPONSES['set_known_color']
 
-        speech_output = responses['speech_output'].format(favorite_color)
-        reprompt_text = responses['reprompt_text']
-        should_end_session = responses['should_end_session']
+        output = RESPONSES['set_known_color']['speech_output']
+        output = output.format(favorite_color)
 
-        response = get_response(card_title, speech_output, reprompt_text,
-                                should_end_session)
+        response = get_response_alt('set_known_color', speech_output=output)
         result = get_lambda_output(response, session_attributes)
     elif code == CODES['no_match']:
         result = build_unformatted_speechlet_response('set_unknown_color')
@@ -323,16 +332,11 @@ def get_color_from_session(intent, session):
     try:
         favorite_color = session['attributes']['favoriteColor']
 
+        output = RESPONSES['get_known_color']['speech_output']
+        output = output.format(favorite_color)
+
+        response = get_response_alt('get_known_color', speech_output=output)
         session_attributes = create_continue_prompt_attributes(favorite_color)
-        responses = RESPONSES['get_known_color']
-
-        card_title = intent['name']
-        speech_output = responses['speech_output'].format(favorite_color)
-        reprompt_text = responses['reprompt_text']
-        should_end_session = responses['should_end_session']
-
-        response = get_response(card_title, speech_output, reprompt_text,
-                                should_end_session)
         result = get_lambda_output(response, session_attributes)
     except KeyError:
         result = build_unformatted_speechlet_response('get_unknown_color')
